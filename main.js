8d7c124e57dc739782b0b7522589a6091410f96e
Jimmy Lin <jimmydlin@gmail.com>
2019-10-14 08:44:07 -0700

    etsy v3 ignore test files

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ var html = items.results.map(item => {
         <div class="image">
             
 
-        <img src="${item.Images[0].url_fullxfull}" height="200px" width="200px">
-
+        <a href="#"><img src="${item.Images[0].url_fullxfull}" height="200px" width="200px">
+        </a>
         </div>
         <div class="overlay">
             <i class="fa fa-heart"></i>
